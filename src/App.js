80ebda0d04ff879619b7b38d102367008d7c90e2
cws0316 <dev.chwonseok@gmail.com>
2021-10-18 16:01:01 +0900

    Downloaded all icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import styled from 'styled-components';
+import CityComponent from './components/CityComponent';
+import WeatherComponent from './components/WeatherComponent';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+
+  width: 375px; // for both PC and Mobile
+  padding: 15px 5px;
+  border-radius: 5px;
+
+  box-shadow: 0 3px 5px 0 #777;
+`;
+
+const Label = styled.div`
+  color: #000;
+
+  font-family: 'Montserrat Alternates', sans-serif;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
 function App() {
-  return <div>Hey</div>;
+  return (
+    <Container>
+      <Label>Weather Finder</Label>
+      <CityComponent />
+      <WeatherComponent />
+    </Container>
+  );
 }
 
 export default App;
